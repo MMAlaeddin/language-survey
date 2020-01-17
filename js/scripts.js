d5f11b5c17ccdc350a1c8781cdfa285453d58b15
Student <example@example.com>
2020-01-17 10:31:17 -0800

    look for bug(s)

--- js/scripts.js
+++ js/scripts.js
@@ -6,32 +6,20 @@ $(document).ready(function(){
     var code4 = $("input:radio[name=code4]:checked").val();
     var code5 = $("input:radio[name=code5]:checked").val();
 
+
     if(code1 === design){
       $("#java").show();
-      $("#sharp").hide();
-      $("#plus").hide();
+      $("sharp").hide();
+      $("plus").hide();
     }else if(code1 === make){
       $("#sharp").show();
       $("#plus").hide();
       $("#java").hide();
-    }else((code1 === game){
+    }else(code1 === game)
       $("#plus").show();
       $("#sharp").hide();
       $("#java").hide();
-    }
-    if(code1 === design && code2 === colors && code3 === no && code4 === mario && code5 === fruits){
-      $("#java").show();
-      $("sharp").hide();
-      $("plus").hide();
-    }else if(code1 === make && code2 === solve && code3 === know && code4 === chun && code5 === nachos){
-      $("#sharp").show();
-      $("#plus").hide();
-      $("#java").hide();
-    }else if(code1 === game && code2 === puzzle && code3 === yes && code4 === arthos && code5 === tacos){
-      $("#plus").show();
-      $("#sharp").hide();
-      $("#java").hide();
-    }
+    
     
   event.preventDefault();
   })
