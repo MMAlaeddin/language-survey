110941b0d284ddb22cf491b9d85cea36e0f0f89e
Student <example@example.com>
2020-01-17 09:11:52 -0800

    add submit button

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#form").submit(function(event){
+    var code1 = $("input:radio[name=code1]:checked").val();
+    var code2 = $("input:radio[name=code2]:checked").val();
+    var code3 = $("input:radio[name=code3]:checked").val();
+    var code4 = $("input:radio[name=code4]:checked").val();
+    var code5 = $("input:radio[name=code5]:checked").val();
+
+    
+  })      
+})
